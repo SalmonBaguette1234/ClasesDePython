8d69532f136322615b696f2e7a9bf020d536a032
SalmonBaguette1234 <118238115+SalmonBaguette1234@users.noreply.github.com>
2022-11-14 20:30:22 -0700

    Clase #1
    Clase pasada xd

---
+++ Clase1.py
@@ -0,0 +1,39 @@
+(2) #INT
+("hola")#STRING
+(2.7) #FLOAT
+(True) #BOLEANO
+
+#-----------------------
+
+VarInt = (2)
+VarString = ("hola")
+VarBol = (True)
+VarFloat = (2.5)
+
+#------------------------
+
+print("hola")
+print(2)
+print(VarString)
+print(VarInt)
+
+#------------------------
+
+input1 = input("ingresa nombre")
+print("hola!" + input1)
+
+#------------------------
+
+if (VarString == "hola"):
+  print("Hola!")
+else:
+  print("?")
+  
+  #-----------------------
+  
+  if (input1 == "queso"):
+   print("?")
+elif (input == "hola"):
+  print("hola")
+else:
+  print("si")
